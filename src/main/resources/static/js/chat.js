c57c130c94ebb6063c30a301aeb896bbf5d918f4
aleksei_shvariov <alxsshv@gmail.com>
2023-01-24 23:47:53 +0300

    Дополнен файл chat.js

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,7 +1,19 @@
 $(function(){
+    let initApplication = function(){
+
+    };
+
+    let registerUser = function(name){
+        $.post("/auth", {name: name}, function(response){
+        })
+    };
+
     $('.send-button').on('click', function(){
         $.get('/init', {}, function(response){
-            alert(response);
+            if(response == false){
+            let name = prompt('Введите Ваше имя:');
+            } else {
+            }
         })
     });
 });
